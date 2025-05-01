85eca99f28e58a92462aac6a178302341e3fd3b1
24DavidM <151885025+24DavidM@users.noreply.github.com>
2025-05-01 15:05:40 -0500

    Continue

--- fundamentals.js
+++ fundamentals.js
@@ -105,4 +105,94 @@ const validarMail = () =>{
     console.log("Mail valido")
 }
 
-r
+// OBJECTOS
+
+let user = {
+    name: "David",
+    lastName:"Muela",
+    address:{
+        city:"Quito",
+        country:"Ecuador"
+    },
+    friends: ["Juan","Pedro","Luis"],
+    age: 20,
+    isActive:true,
+    password: "123"
+}
+
+user.avatar = "https://avatar.com"
+
+// ELIMIAR UN OBJECTO DE OBJECTO
+delete user.password
+
+console.log(user)
+console.log(user.password)
+
+
+// ES6 - Destructuracion de objectos
+
+const {name, lastName, address:direccion,age,avatar} = user
+console.log(name)
+console.log(lastName)
+console.log(direccion.country)
+
+const otherInformation = {
+    status : "active",
+    phone : "0939564049",
+    friends: ["Juan","Pedro","Luis"],
+    sendMail(){
+        console.log(`Send mail to ${otherInformation.friends[0]}`)
+    }
+}
+
+console.log(otherInformation.sendMail())
+
+
+//ES6 - Operador de propagación - spread operator (....)
+const allInformation = {
+    ...user,
+    ...otherInformation
+}
+
+console.log(allInformation)
+
+console.log(Object.keys(user).includes(""))
+
+// FUNCTION - CALLBACK
+
+function mostrarBienvenido (nombre){
+    console.log(`Bienvenido ${nombre}`)
+}
+
+function registrarUsuario(callback){
+    const user = "David"
+    callback(user)
+}
+
+registrarUsuario(mostrarBienvenido)
+
+
+//ARRAYS
+
+const products = ["Play","Pc","Monitor","Teclado"]
+const prices = [1000,2000,3000,4000]
+
+products.forEach((producto,index) =>{console.log(index+1,producto)})
+
+
+const newProductsOne = products.map((producto)=>{
+    return producto.toUpperCase()
+})
+
+
+
+const newProductsTwo = products.map((producto)=>(
+    producto.toUpperCase()
+))
+
+
+const newProductsThree = products.map((producto)=> producto.toUpperCase())
+
+console.log(newProductsOne)
+console.log(newProductsTwo)
+console.log(newProductsThree)
