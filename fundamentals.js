f831e47078a595ecf83505bbd9ce289f38fc0e45
24DavidM <151885025+24DavidM@users.noreply.github.com>
2025-05-01 15:37:28 -0500

    Metodos

--- fundamentals.js
+++ fundamentals.js
@@ -196,3 +196,40 @@ const newProductsThree = products.map((producto)=> producto.toUpperCase())
 console.log(newProductsOne)
 console.log(newProductsTwo)
 console.log(newProductsThree)
+
+//FILTRAR 
+
+const items = products.filter(producto => producto !== "Pc")
+console.log(items)
+
+// CONCATENEAR
+
+const newItems = [...products,...prices]
+console.log(newItems)
+
+//BUSCAR
+
+const findItem = products.find(producto => producto === "Pc")
+findItem ? console.log(`Producto encontrado: ${findItem}`) : console.log("Producto no encontrado :(")
+
+
+// Agrega un elemento al final del array
+products.push()
+// Agrega un elemento al principio del array.
+products.unshift()
+
+
+//Elimina el último elemento del array y lo devuelve.
+products.pop()
+//Elimina el primer elemento del array y lo devuelve.
+
+
+products.shift()
+
+
+products.includes("Mouse") ? console.log("Existe") : console.log("No existe")
+
+products.length === 3 ? console.log("50% Descuento") : console.log("No hay descuento")
+
+const newItemsSlice = products.slice(0,2)
+console.log(newItemsSlice)
